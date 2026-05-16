2108f5c885788032f964e19dc2ef9129a3ca422e
Yonn <yonn.apps@outlook.com>
2026-05-17 00:19:08 +0200

    added simple ping command to test command loader

---
+++ src/Commands/ping.ts
@@ -0,0 +1,27 @@
+import {
+    SlashCommandBuilder,
+    TextDisplayBuilder,
+    ContainerBuilder,
+    ChatInputCommandInteraction,
+    MessageFlags
+} from "discord.js"
+
+export const data = new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with Pong!");
+
+export async function execute(interaction: ChatInputCommandInteraction) {
+    const TextComponent = new TextDisplayBuilder()
+        .setContent("## Pong!");
+    const container = new ContainerBuilder()
+        .addTextDisplayComponents(TextComponent)
+
+    try {
+        await interaction.reply({
+            components: [container],
+            flags: MessageFlags.IsComponentsV2
+         });
+     } catch (error) {
+         console.error("Error:", error);
+        }
+}
