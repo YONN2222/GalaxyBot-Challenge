14ee1cc503c90da877d86d7daa787b36c86440de
Yonn <yonn.apps@outlook.com>
2026-05-19 18:56:18 +0200

    Added Create Incident Modal

--- src/Commands/incident/create.ts
+++ src/Commands/incident/create.ts
@@ -1,13 +1,10 @@
 import {
-    ChannelSelectMenuBuilder,
-    ChannelType,
     ChatInputCommandInteraction,
     LabelBuilder,
     MessageFlags,
     ModalBuilder,
-    RoleSelectMenuBuilder,
     SlashCommandSubcommandBuilder,
-    type ModalSubmitInteraction, TextDisplayBuilder, ContainerBuilder,
+    type ModalSubmitInteraction, TextDisplayBuilder, ContainerBuilder, TextInputStyle, TextInputBuilder
 } from "discord.js";
 import { Incident } from "../../Database/Models/Incident";
 import { Config } from "../../Database/Models/Config";
@@ -30,5 +27,38 @@ export async function execute(interaction: ChatInputCommandInteraction) {
     // config check
     if (!await ConfigCheck(interaction, config)) return;
 
+    // create modal
+    const modal = new ModalBuilder()
+        .setCustomId("create-incident-modal")
+        .setTitle("Create Incident");
 
+    const titleInput = new LabelBuilder()
+        .setLabel("Title:")
+        .setDescription("Enter the title of the incident.")
+        .setTextInputComponent(
+            new TextInputBuilder()
+                .setCustomId("incident-title")
+                .setPlaceholder("Incident Title")
+                .setRequired(true)
+                .setMinLength(1)
+                .setMaxLength(100)
+                .setStyle(TextInputStyle.Short)
+        );
+
+    const descriptionInput = new LabelBuilder()
+        .setLabel("Description:")
+        .setDescription("Enter a description of the incident.")
+        .setTextInputComponent(
+            new TextInputBuilder()
+                .setCustomId("incident-description")
+                .setPlaceholder("Incident Description")
+                .setRequired(true)
+                .setMinLength(1)
+                .setMaxLength(1000)
+                .setStyle(TextInputStyle.Paragraph)
+        );
+
+    modal.addLabelComponents(titleInput, descriptionInput);
+
+    await interaction.showModal(modal);
 }
